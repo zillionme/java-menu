7c13f06c6f163e7876578fb29d7869062cafe187
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2022-12-17 16:55:34 +0900

    refactor : 이름 입력과 동시에 코치 생성 기능 추가(수정)

--- src/main/java/menu/Controller.java
+++ src/main/java/menu/Controller.java
@@ -1,10 +1,10 @@
 package menu;
 
 
-import menu.constants.Category;
-import menu.constants.DayOfWeek;
 import menu.domain.Coach;
 import menu.domain.MenuService;
+import menu.domain.constants.Category;
+import menu.domain.constants.DaysOfWeek;
 import menu.view.InputView;
 import menu.view.OutputView;
 
@@ -17,61 +17,21 @@ public class Controller {
 
     public void generate() {
         outputView.printStart();
-        //메뉴 추천 서비스 생성 with 코치들
         MenuService menuService = createMenuService();
-        //메뉴 추천하기
-        recommendMenu(menuService);
-        //결과 출력하기
+
+        menuService.recommendMenu();
         printResult(menuService);
+
         outputView.printEnd();
     }
 
     public MenuService createMenuService() {
-        List<String> coachNames = getCoachNames();
-        List<Coach> coaches = getCoachList(coachNames);
+        List<Coach> coaches = getCoaches();
         addUnableToEatMenusToCoaches(coaches);
 
         return new MenuService(coaches);
     }
 
-    public void recommendMenu(MenuService menuService) {
-        for (int i = 0; i < 5; i++) {
-            menuService.addCategory(); //요일 추가하면 더 좋을 것 같음. map으로 요일별 카테고리
-            menuService.addTodayMenuToCoaches(); // 각 코치들에게 메뉴 추천하기
-
-        }
-
-    }
-
-    public void printResult(MenuService menuService) {
-        // 구분 ,요일
-        List<String> daysOfWeek = DayOfWeek.getDaysOfWeek();
-        // 카테고리, 카테고리들
-        List<String> categories = menuService.getRecommendCategories();
-        // 코치 이름과 메뉴들
-        List<String> menusOfCoach = menuService.getRecommendMenus();
-        outputView.printResult(daysOfWeek, categories, menusOfCoach);
-    }
-
-    public List<String> getCoachNames() {
-        try {
-            return inputView.readCoachNames();
-
-        } catch (IllegalArgumentException e) {
-            outputView.printError(e.getMessage());
-        }
-        return getCoachNames();
-    }
-
-    public List<Coach> getCoachList(List<String> coachNames) {
-        List<Coach> coaches = new ArrayList<>();
-        for (String coachName : coachNames) {
-            coaches.add(new Coach(coachName));
-        }
-        return coaches;
-    }
-
-    //있는 음식인지 validate해야함
     public void addUnableToEatMenusToCoaches(List<Coach> coaches) {
         for (Coach coach : coaches) {
             List<String> unableToEatMenus = getUnableToEatMenus(coach.getName());
@@ -79,17 +39,35 @@ public class Controller {
         }
     }
 
-    //메뉴가 리스트에 있는지도 확인해야함.
+    public List<Coach> getCoaches() {
+        try {
+            List<String> coachNames = inputView.readCoachNames();
+            return creatCoachList(coachNames);
+
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+        }
+        return getCoaches();
+    }
+
     public List<String> getUnableToEatMenus(String coachName) {
         try {
             List<String> unableToEatMenus = inputView.readUnableToEatMenus(coachName);
-            Category.validateMenu(unableToEatMenus);
+            Category.validateMenu(unableToEatMenus); //카테고리에 있는 메뉴인지 확인
             return unableToEatMenus;
 
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
         }
         return getUnableToEatMenus(coachName);
-
     }
+
+    public List<Coach> creatCoachList(List<String> coachNames) {
+        List<Coach> coaches = new ArrayList<>();
+        for (String coachName : coachNames) {
+            coaches.add(new Coach(coachName));
+        }
+        return coaches;
+    }
+
 }
