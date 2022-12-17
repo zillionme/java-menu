f87fe57ece1cc4b9336e176d4e9f0c26da302d82
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2022-12-17 16:45:42 +0900

    refactor : 추천 메뉴 결과 출력 기능 수정

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -5,29 +5,33 @@ import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String MESSAGE_START = "점심 메뉴 추천을 시작합니다.\n";
-    private static final String MESSAGE_RECOMMENDATION = "메뉴 추천 결과입니다.";
-    private static final String MESSAGE_END = "추천을 완료했습니다.";
-
+    private static final String MESSAGE_RECOMMENDATION = "\n메뉴 추천 결과입니다.";
+    private static final String RESULT_DELIMITER = " | ";
+    private static final String RESULT_PREFIX = "[ ";
+    private static final String RESULT_SUFFIX = " ]";
+    private static final String MESSAGE_END = "\n추천을 완료했습니다.";
 
     public void printStart() {
         System.out.println(MESSAGE_START);
-
     }
 
     public void printError(String errorMessage) {
         System.out.println(errorMessage);
     }
 
-    public void printResult(List<String> daysOfWeek, List<String> recommendCategories, List<String> menusOfCoach) {
-        String days = daysOfWeek.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
-        String categories = recommendCategories.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+    public void printResult(List<String> daysOfWeek, List<String> recommendCategories,
+                            List<List<String>> menusOfCoaches) {
         System.out.println(MESSAGE_RECOMMENDATION);
-        System.out.println(days);
-        System.out.println(categories);
-        //코치는 그냥 출력
-        for (String menuOfCoach : menusOfCoach) {
-            System.out.println(menuOfCoach);
+        System.out.println(standardiseResult(daysOfWeek));
+        System.out.println(standardiseResult(recommendCategories));
+
+        for (List<String> menuOfCoach : menusOfCoaches) {
+            System.out.println(standardiseResult(menuOfCoach));
         }
+    }
+
+    public String standardiseResult(List<String> input) {
+        return input.stream().collect(Collectors.joining(RESULT_DELIMITER, RESULT_PREFIX, RESULT_SUFFIX));
     }
 
     public void printEnd() {
