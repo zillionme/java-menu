1b40a802280e7b7ff1f96bf264e0e333bcff133b
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2022-12-17 15:09:00 +0900

    feat : 에러 메시지 반환 기능 추가

---
+++ src/main/java/menu/util/ErrorCode.java
@@ -0,0 +1,22 @@
+package menu.util;
+
+public enum ErrorCode {
+    NOT_VALID_INPUT("값이 입력되지 않았습니다"),
+    NOT_NUMBER("숫자를 입력하지 않았습니다"),
+    NOT_VALID_COACH_COUNT_MIN("코치는 최소 2명 이상 입력해야 합니다."),
+    NOT_VALID_COACH_COUNT_MAX("코치는 최대 5명 이하로 입력해야 합니다."),
+    NOT_VALID_COACH_NAME("코치 이름은 최소 2글자, 최대 4글자를 입력해야 합니다."),
+    NOT_VALID_UNABLE_MENU_COUNT("못먹는 메뉴는 최대 2개만 입력해야 합니다.");
+
+
+    private static final String ERROR_BEGIN = "[ERROR] ";
+    private final String errorMessage;
+
+    ErrorCode(String errorMessage) {
+        this.errorMessage = ERROR_BEGIN + errorMessage;
+    }
+
+    public IllegalArgumentException throwError() {
+        return new IllegalArgumentException(this.errorMessage);
+    }
+}
