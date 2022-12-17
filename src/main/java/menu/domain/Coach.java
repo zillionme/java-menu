1cb7e587378683111d8dd75b0382e73e3d5b7e07
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2022-12-17 16:51:32 +0900

    refactor : 코치 이름 유효성 검사 기능 위치 및 추천 메뉴 추가 기능 수정

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -4,44 +4,43 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static menu.util.ErrorCode.NOT_VALID_COACH_NAME;
 
 public class Coach {
+    private static final int COACH_NAME_LENGTH_MIN = 2;
+    private static final int COACH_NAME_LENGTH_MAX = 4;
+    private final List<String> MenuOfDays = new ArrayList<>();
     private final String name;
     private List<String> unableToEatMenus;
-    private String todayMenu;
-    private List<String> MenuOfDays = new ArrayList<>();
 
     public Coach(String name) {
+        validateCoachName(name);
         this.name = name;
+    }
+
+    public void validateCoachName(String input) {
+        if (input.length() < COACH_NAME_LENGTH_MIN || input.length() > COACH_NAME_LENGTH_MAX) {
+            throw NOT_VALID_COACH_NAME.throwError();
+        }
     }
 
     public void setUnableToEatMenus(List<String> unableToEatMenus) {
         this.unableToEatMenus = unableToEatMenus;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getRecommendMenuOfDays() {
-        List<String> recommendMenus = new ArrayList<>();
-        recommendMenus.add(name);
-        for (String menuOfDay : MenuOfDays) {
-            recommendMenus.add(menuOfDay);
-        }
-        return recommendMenus.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
-    }
-
-    public void addMenuOfDays(List<String> menu) {
+    public void addMenuOfDaysWith(List<String> menu) {
+        String todayMenu;
         do {
             todayMenu = Randoms.shuffle(menu).get(0);
         } while (!isPossibleMenuToAdd(todayMenu));
+
         MenuOfDays.add(todayMenu);
     }
 
+    //유효성 검사
     public boolean isPossibleMenuToAdd(String menu) {
-        return isEatableMenu(menu) || isNotDuplicatedMenu(menu);
+        return isEatableMenu(menu) && isNotDuplicatedMenu(menu);
     }
 
     public boolean isEatableMenu(String menu) {
@@ -49,6 +48,20 @@ public class Coach {
     }
 
     public boolean isNotDuplicatedMenu(String menu) {
-        return !MenuOfDays.contains(menu);
+        return !MenuOfDays.contains(menu); //이미 중복되지 않았는지.
     }
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getRecommendMenuOfDays() {
+        List<String> recommendMenus = new ArrayList<>();
+        recommendMenus.add(name);
+        recommendMenus.addAll(MenuOfDays);
+
+        return recommendMenus;
+    }
+
 }
