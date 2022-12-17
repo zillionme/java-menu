97b4bbf9ab55f93e2e65787198bd6c213ce6bb75
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2022-12-17 15:28:53 +0900

    feat : 카테고리에 없는 메뉴 입력 시 예외 발생 기능 추가

--- src/main/java/menu/util/ErrorCode.java
+++ src/main/java/menu/util/ErrorCode.java
@@ -6,7 +6,8 @@ public enum ErrorCode {
     NOT_VALID_COACH_COUNT_MIN("코치는 최소 2명 이상 입력해야 합니다."),
     NOT_VALID_COACH_COUNT_MAX("코치는 최대 5명 이하로 입력해야 합니다."),
     NOT_VALID_COACH_NAME("코치 이름은 최소 2글자, 최대 4글자를 입력해야 합니다."),
-    NOT_VALID_UNABLE_MENU_COUNT("못먹는 메뉴는 최대 2개만 입력해야 합니다.");
+    NOT_VALID_UNABLE_MENU_COUNT("못먹는 메뉴는 최대 2개만 입력해야 합니다."),
+    NOT_VALID_MENU("카테고리에 해당하는 메뉴가 없습니다.");
 
 
     private static final String ERROR_BEGIN = "[ERROR] ";
