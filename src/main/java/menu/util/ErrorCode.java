e29914e994a7f33e51a10024bcc8245b081decf2
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2022-12-17 16:52:32 +0900

    feat : 해당 음식 카테고리가 없는 경우 추가

--- src/main/java/menu/util/ErrorCode.java
+++ src/main/java/menu/util/ErrorCode.java
@@ -2,7 +2,7 @@ package menu.util;
 
 public enum ErrorCode {
     NOT_VALID_INPUT("값이 입력되지 않았습니다"),
-    NOT_NUMBER("숫자를 입력하지 않았습니다"),
+    NOT_VALID_CATEGORY("해당하는 음식 카테고리가 없습니다."),
     NOT_VALID_COACH_COUNT_MIN("코치는 최소 2명 이상 입력해야 합니다."),
     NOT_VALID_COACH_COUNT_MAX("코치는 최대 5명 이하로 입력해야 합니다."),
     NOT_VALID_COACH_NAME("코치 이름은 최소 2글자, 최대 4글자를 입력해야 합니다."),
